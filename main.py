9623541afcedb9919ec8d4cb67cb05d9d4e0647b
Adrien RICHARD <richardadrien0@gmail.com>
2020-12-19 23:54:50 +0100

    custom mods

--- main.py
+++ main.py
@@ -21,43 +21,48 @@ def images_annotations_info(maskpath):
     start = 1
     for image_id, mask_image in enumerate(mask_images, 1):
         file_name = os.path.basename(mask_image)#.split('.')[0] + ".jpg"
-        print("treating mask "+str(start)+" / "+str(number_of_masks) +" ("+str(round(start/number_of_masks*100,1))+"%"+") " + file_name)
-        start = start + 1
-        
-        # image shape
-        mask_image_open = Image.open(mask_image)
-        w, h = mask_image_open.size
-        
-        # 'images' info 
-        image = create_image_annotation(file_name, w, h, image_id)
-        images.append(image)
+        if file_name in ignore_files:
+            print(file_name + " is in ignore list")
+        else:
+            print("treating mask "+str(start)+" / "+str(number_of_masks) +" ("+str(round(start/number_of_masks*100,1))+"%"+") " + file_name)
+            try:
+                start = start + 1
+                
+                # image shape
+                mask_image_open = Image.open(mask_image)
+                w, h = mask_image_open.size
+                
+                # 'images' info 
+                image = create_image_annotation(file_name, w, h, image_id)
+                images.append(image)
 
-        sub_masks = create_sub_masks(mask_image_open, w, h)
-        for color, sub_mask in sub_masks.items():
-            category_id = category_ids[color]
+                sub_masks = create_sub_masks(mask_image_open, w, h)
+                for color, sub_mask in sub_masks.items():
 
-            # 'annotations' info
-            polygons, segmentations = create_sub_mask_annotation(sub_mask)
+                    category_id = category_ids[color]
+                    # 'annotations' info
+                    polygons, segmentations = create_sub_mask_annotation(sub_mask)
 
-            # Three labels are multipolygons in our case: wall, roof and sky
-            if(category_id == 2 or category_id == 5 or category_id == 6):
-                # Combine the polygons to calculate the bounding box and area
-                multi_poly = MultiPolygon(polygons)
-                                
-                annotation = create_annotation_format(multi_poly, segmentations, image_id, category_id, annotation_id)
-
-                annotations.append(annotation)
-                annotation_id += 1
-            else:
-                for i in range(len(polygons)):
-                    # Cleaner to recalculate this variable
-                    segmentation = [np.array(polygons[i].exterior.coords).ravel().tolist()]
-                    
-                    annotation = create_annotation_format(polygons[i], segmentation, image_id, category_id, annotation_id)
-                    
-                    annotations.append(annotation)
-                    annotation_id += 1
-        
+                    # multipolygon categories
+            #     if(category_id == 0 or category_id == 2 or category_id == 4):
+            #         # Combine the polygons to calculate the bounding box and area
+            #         multi_poly = MultiPolygon(polygons)
+            #                         
+            #         annotation = create_annotation_format(multi_poly, segmentations, image_id, category_id, annotation_id)
+            #
+            #     annotations.append(annotation)
+            #         annotation_id += 1
+            #     else:
+                    for i in range(len(polygons)):
+                        # Cleaner to recalculate this variable
+                        segmentation = [np.array(polygons[i].exterior.coords).ravel().tolist()]
+                        
+                        annotation = create_annotation_format(polygons[i], segmentation, image_id, category_id, annotation_id)
+                        
+                        annotations.append(annotation)
+                        annotation_id += 1
+            except:
+                print("An exception occurred") 
     return images, annotations
 
 if __name__ == '__main__':
